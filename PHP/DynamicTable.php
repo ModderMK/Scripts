2d48357858cf06e2dd5799c2300ea183a08d1bb3
Modder <63869963+ModderMK@users.noreply.github.com>
2023-11-08 10:56:30 -0300

    Att

---
+++ PHP/DynamicTable.php
@@ -0,0 +1,37 @@
+<?php 
+    include './conexao.php';
+    
+    $query = "";
+    $result = oci_parse($conn_ora, $query);
+    oci_execute($result);
+
+    $columns = [];
+    $columnCount = oci_num_fields($result);
+
+    for ($i = 1; $i <= $columnCount; $i++) {
+        $columnName = oci_field_name($result, $i);
+        $columns[] = $columnName;
+    }
+?> 
+
+<table cellspacing="0" cellpadding="0">
+    <thead>
+        <?php
+            foreach ($columns as $column) {
+                echo '<th style="text-align: center;">' . $column . '</th>';
+            }
+        ?>
+    </thead>
+
+    <tbody>
+        <?php
+            while($row = oci_fetch_array($result)){
+                echo '<tr>';
+                    foreach ($columns as $column) {
+                        echo '<td style="text-align: center;">' . $row[$column] . '</td>';
+                    }
+                echo '</tr>';
+            }
+        ?>    
+    </tbody>              
+</table>
